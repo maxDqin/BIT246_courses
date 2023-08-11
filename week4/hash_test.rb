840ffce9e05a1a174586d8e58ae5da23184d9c66
maxDqin <max.qin1@gmail.com>
2023-08-12 00:25:36 +1000

    rm week4/*

--- week4/hash_test.rb
+++
@@ -1,9 +0,0 @@
-# user = { name: " Jone Simth", email: "John@example.com" }
-# user = { "name" => "Jone Simth", "email" => "john1@example.com" }
-# user = { :name => " Jone Simth", :email => "John@example.com" }
-#
-# puts user[:name]
-
-kids = {"first" => "Alex", "second" => "Will", "third" => "Molly"}
-kids.delete_if {|key, value| key >= "third"}
-puts kids
